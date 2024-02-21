fcb746f359c117ae5d5261a2b2e12b1c3f663be6
amirkhan0102 <amriddinjavqochdiyev@gmail.com>
2024-02-21 16:48:30 +0500

    added file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { DataServiceService } from './data.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'http';
+  data: any;
+  apiUrl: any;
+  http: any;
+  constructor(private dataService: DataServiceService) {
+
+  }
+  ngOnInit(): void {
+    this.getData()
+  }
+
+
+  getData() {
+    this.dataService.getData().subscribe(Response => {
+      this.data = Response;
+      console.log(this.data);
+
+    }
+    )
+  }
+  createPost() {
+    const newPost = {
+      title: "yangi post",
+      body: "Amriddin ",
+      userId: 1,
+    };
+
+
+
+    this.dataService.createPost(newPost).subscribe(Response => {
+      console.log("yangi post yaratildi", Response);
+
+    }, error => {
+      console.error("Xatolik bo'ldi",error);
+
+    })
+  }
+
+  updatePost(postId: number) {
+    const updatePost = {
+      title: "ma'lumot o'zgartirildi",
+      body: "ma'lumot o'zgartirildi",
+    };
+    this.dataService.updatePost(postId, updatePost).subscribe(Response => {
+      console.log(" post o'zgartirildi", Response);
+
+    }, error => {
+      console.error("Xatolik bo'ldi", error);
+
+    })
+
+  }
+
+  deletePost(postId: number) {
+
+    this.dataService.deletePost(postId,).subscribe(Response => {
+      console.log(" post o'zgartirildi", Response);
+    })
+
+
+
+  }
 }
---
+++ src/app/data.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataServiceService {
+
+
+
+  private apiUrl = 'https://jsonplaceholder.typicode.com/posts'
+
+  constructor(private http: HttpClient) { }
+
+
+
+  getData(): Observable<any> {
+    return this.http.get<any>(this.apiUrl)
+  }
+
+
+  createPost(postData: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, postData)
+  }
+
+
+
+  updatePost(postId: number, postData: any): Observable<any> {
+  
+    const url =`${this.apiUrl}/ ${postId}`;
+    return this.http.put<any>(url, postData);
+
+  }
+
+
+  deletePost(postId: number): Observable<any> {
+    const url = `${this.apiUrl} / ${postId}`;
+    return this.http.delete(url);
+  }
+
+
+
+
+}
